0043e196e7661a3057c8010b4b2e824efb74c6d9
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2024-04-24 21:46:45 +0200

    more scaffolding

--- src/main.py
+++ src/main.py
@@ -1,25 +1,39 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
 class ShortenRequest(BaseModel):
-    url: str
+    url: str | None = None
     shortcode: str | None = None
 
 @app.post("/shorten")
 def create_short_url(shorten_request: ShortenRequest):
+    if shorten_request.url is None:
+        raise HTTPException(status_code=400, detail="Url not present")
+    
+    # if shorten_request.shortcode is invalid:
+    #     raise HTTPException(status_code=412, detail="The provided shortcode is invalid")
+
+
+    # if shorten_request.shortcode is alreadyinuse:
+        # raise HTTPException(status_code=409, detail="Shortcode already in use")
+
     return shorten_request
 
 @app.get("/{shortcode}")
-def read_item(shortcode: str):
+def read_shortcode(shortcode: str):
+    # if shortcode is notfound:
+    #     raise HTTPException(status_code=404, detail="Shortcode not found")
+
     return shortcode
 
 @app.get("/{shortcode}/stats")
-def read_item(shortcode: str):
-    return shortcode
+def read_stats(shortcode: str):
+    # if shortcode is notfound:
+    #     raise HTTPException(status_code=404, detail="Shortcode not found")
+
+    return shortcode + ' stats'
 
 @app.get("/")
 def read_root():
